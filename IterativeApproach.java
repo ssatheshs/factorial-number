0055512f5a69237d40cd34af6c8026eec762e7b5
satheesh <sekarsatheesh30@gmail.com>
2024-01-07 12:30:35 +0530

    Factorial using Iterative approach.

--- IterativeApproach.java
+++ IterativeApproach.java
@@ -1,13 +1,22 @@
 public class IterativeApproach {
     public static void main(String[] args) {
         System.out.println("Factorial using Iterative approach");
-        int num = 4;
+        int num = 5;
         System.out.println(String.format("Factorial of %d is %d ", num, calculateFactorial(num)));
+        System.out.println(String.format("Factorial of %d is %d ", num, calculateFact(num)));
     }
 
     private static int calculateFactorial(int num){
         int res = 1;
         for(int i = 2; i <= num; i++){
+            res *= i;
+        }
+        return res;
+    }
+
+    private static int calculateFact(int num){
+        int res = 1;
+        for(int i = num; i >= 2; i--){
             res *= i;
         }
         return res;
